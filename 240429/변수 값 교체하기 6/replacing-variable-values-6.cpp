584916d8e20cd04e2d7195565696fddf4475cca1
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-04-29 20:18:50 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240429/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 2;
+    int b = 5;
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << a << endl << b;
+    return 0;
+}
