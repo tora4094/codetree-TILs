d63c7fd29cda5b378990ffbbe77100bb45e9a0e8
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-04-29 20:59:46 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240429/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a,b;    
+    char c;
+    cin >> c >> a >> b;
+    cout << fixed;
+    cout.precision(2);
+    cout << c << endl << a << endl << b;
+
+    return 0;
+}
