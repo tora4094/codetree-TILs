105654a48d15e5493a4a92b0474b546ca9460e17
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-04-29 21:12:12 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240429/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int a,b,c;
+    char d;
+    cin >> a >> d >> b >> d >> c;
+    cout << 0 << a << '-' << c << '-' << b;
+    return 0;
+}
