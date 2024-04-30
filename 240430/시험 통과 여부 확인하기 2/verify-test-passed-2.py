692b5979074a8b7ede5d257395f1fda39f5e965b
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-04-30 11:22:44 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240430/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+count = 0
+for _ in range(0, n):
+    arr = list(map(int,input().split()))
+    avg = sum(arr) / 4.0
+    if (avg >= 60):
+        print("pass")
+        count += 1
+    else:
+        print("fail")
+print(count)
