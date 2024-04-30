1eb0ea7345a93bd1d9e381933340b8b8be64b4dc
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-04-30 10:42:39 +0900

    Title: 학점 계산기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240430/학점 계산기/credit-calculator.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(float,input().split()))
+sum_val = sum(arr)
+avg_val = sum_val / n
+print(f"{avg_val:.1f}")
+
+if (avg_val >= 4.0):
+    print("Perfect")
+elif (avg_val >= 3.0):
+    print("Good")
+else:
+    print("Poor")
