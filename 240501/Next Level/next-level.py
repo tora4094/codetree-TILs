ebf4e749d49e482dc2204fd47b5e90f2b365045f
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-01 20:34:18 +0900

    Title: Next Level, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/Next Level/next-level.py
+++ 240501/Next Level/next-level.py
@@ -1,13 +1,20 @@
 class User:
-    def __init__(self,id,level=1):
+    def __init__(self,id="",level=1):
         self.id = id
         self.level = level
     def info(self):
         print(f"user {self.id} lv {self.level}")
 
+user1 = User()
+user2 = User()
+
+user1.id = "codetree"
+user1.level = 10
+
 id, level = tuple(input().split())
-user1 = User("codetree",10)
-user2 = User(id,int(level))
+
+user2.id = id
+user2.level = int(level)
 
 user1.info()
 user2.info()
