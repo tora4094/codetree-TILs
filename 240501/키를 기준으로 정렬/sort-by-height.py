1b31e9aad09f47a3bc81da8cc33fbd34c7dcdd6e
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-01 21:10:59 +0900

    Title: 키를 기준으로 정렬, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240501/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(self,name,height,weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+n = int(input())
+
+students = []
+for _ in range(n):
+    name,height,weight = tuple(input().split())
+    students.append(Student(name,int(height),int(weight)))    
+
+students.sort(key=lambda x: x.height)
+
+for student in students:
+    print(student.name, student.height, student.weight)
