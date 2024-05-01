9b72b94f8eef9e87e3afa48d5793fa22d02b6445
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-01 14:48:37 +0900

    Title: 배열의 평균, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/배열의 평균/ave-of-array.py
@@ -0,0 +1,27 @@
+n = 2 
+arr = []
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+
+for i in range(n):
+    sum_val = 0 
+    for j in range(4):
+        sum_val += arr[i][j]
+    h_avg = sum_val / 4
+    print(f"{h_avg:.1f}",end=" ")
+print()
+
+for i in range(4):
+    sum_val = 0
+    for j in range(n):
+        sum_val += arr[j][i]
+    v_avg = sum_val / 2
+    print(f"{v_avg:.1f}",end=" ")
+print()
+
+sum_val = 0
+for i in range(n):
+    for j in range(4):
+        sum_val += arr[i][j]
+avg = sum_val / (n*4)
+print(f"{avg}")
