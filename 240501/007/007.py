bc81e9cfbdb5dcda39a961fb4071fa2b233a58e3
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-01 20:12:11 +0900

    Title: 007, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/007/007.py
+++ 240501/007/007.py
@@ -3,8 +3,9 @@ class agent:
         self.secret_code = secret_code
         self.meeting_point = meeting_point
         self.time = time
-arr = list(input().split())
-agent1 = agent(arr[0],arr[1],arr[2])
+
+secret_code, meeting_point, time = tuple(input().split())
+agent1 = agent(secret_code, meeting_point, time)
 print(f"secret code : {agent1.secret_code}")
 print(f"meeting point : {agent1.meeting_point}")
 print(f"time : {agent1.time}")
