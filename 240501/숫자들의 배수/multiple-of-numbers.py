6c8bc9677ac13f3e48e56f434192dc0435278101
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-01 11:34:24 +0900

    Title: 숫자들의 배수, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/숫자들의 배수/multiple-of-numbers.py
+++ 240501/숫자들의 배수/multiple-of-numbers.py
@@ -5,12 +5,17 @@ arr = [n]
 count_val = 0
 
 for i in count(2):
+    if count_val == 2:
+        break
     current_val = n * i
     arr.append(current_val)
     if current_val % 5 == 0:
         count_val += 1
-    if count_val == 2:
-        break
+    
+    
+
+    
+    
 
 for elem in arr:
     print(elem, end=" ")
