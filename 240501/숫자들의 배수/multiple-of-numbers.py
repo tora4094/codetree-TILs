465a8a9d02e72b67849694a382467b4cb49fc10b
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-01 11:36:50 +0900

    Title: 숫자들의 배수, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/숫자들의 배수/multiple-of-numbers.py
+++ 240501/숫자들의 배수/multiple-of-numbers.py
@@ -1,21 +1,16 @@
 from itertools import count
 n = int(input())
-arr = [n]
+arr = []
 
 count_val = 0
 
-for i in count(2):
-    if count_val == 2:
-        break
+for i in count(1):    
     current_val = n * i
     arr.append(current_val)
     if current_val % 5 == 0:
         count_val += 1
+        if count_val == 2:
+            break
     
-    
-
-    
-    
-
 for elem in arr:
     print(elem, end=" ")
