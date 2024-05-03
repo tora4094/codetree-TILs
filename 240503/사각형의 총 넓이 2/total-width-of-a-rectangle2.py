79fb3c37e89e47b99afc9d4b7f8bdd72e65765c8
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-03 21:31:12 +0900

    Title: 사각형의 총 넓이 2, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240503/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -5,11 +5,12 @@ n = int(input())
 A = [[0 for _ in range(size)] for _ in range(size)]
 
 for _ in range(n):
-    x1,y1,x2,y2 = map(int,input().split())
-    x1 += OFFSET
-    y1 += OFFSET
-    x2 += OFFSET
-    y2 += OFFSET
+    x1, y1, x2, y2 = map(lambda x: int(x)+OFFSET, input().split())
+    # x1,y1,x2,y2 = map(int,input().split())
+    # x1 += OFFSET
+    # y1 += OFFSET
+    # x2 += OFFSET
+    # y2 += OFFSET
 
     for i in range(x1, x2):
         for j in range(y1, y2):
