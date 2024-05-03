769a8d9a2bdc64075bdbf69ac6ae92029ebb0edc
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-03 19:31:33 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240503/Date to Date/date-to-date.py
@@ -0,0 +1,15 @@
+m1, d1, m2, d2 = map(int,input().split())
+
+month_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+if m1 == m2:
+    print(d2 - d1 + 1)
+
+else:
+    total_days = 0
+    total_days += (month_days[m1] - d1 + 1)
+    total_days += d2
+    for month in range(m1 + 1, m2):
+        total_days += month_days[month]
+
+print(total_days)
