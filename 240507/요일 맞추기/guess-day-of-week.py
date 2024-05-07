ed3c4a3143d2746b51d00fb5c27b9f31116a2732
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-07 11:54:15 +0900

    Title: 요일 맞추기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240507/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,25 @@
+m1, d1, m2, d2 = tuple(map(int,input().split()))
+
+month_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+weekdays = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+# 1월 1일 월요일
+start_weekday = 1
+
+def count_days(m1, d1, m2, d2):
+    days = 0
+    if m1 == m2:
+        days = d2 - d1
+    else:
+        days += (month_days[m1] - d1 + 1)
+        for m in range(m1 + 1, m2):
+            days += month_days[m]
+
+        days += (d2 - 1)
+    return days
+
+days = count_days(m1, d1, m2, d2)
+
+weekday_index = (start_weekday + days) % 7
+weekday = weekdays[weekday_index]
+print(weekday)
