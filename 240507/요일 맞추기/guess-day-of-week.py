7a664bcceb3390460179cc4f4e73cea6e90ce8b0
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-07 13:03:16 +0900

    Title: 요일 맞추기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/요일 맞추기/guess-day-of-week.py
+++ 240507/요일 맞추기/guess-day-of-week.py
@@ -1,24 +1,20 @@
 m1, d1, m2, d2 = tuple(map(int,input().split()))
 
-month_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+
+def num_of_days(m, d):
+    days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+    total_days = 0
+
+    for i in range(1,m):
+        total_days += days[i]
+    
+    total_days += d1
+    return total_days
+
+diff = num_of_days(m2,d2) - num_of_days(m1,d1)
+while diff < 0:    
+    diff += 7
 
 weekdays = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
-# 1월 1일 월요일
-start_weekday = 1
-
-def count_days(m1, d1, m2, d2):
-    days = 0
-    if m1 == m2:
-        days = d2 - d1
-    else:
-        days += (month_days[m1] - d1)
-        for m in range(m1 + 1, m2):
-            days += month_days[m]
-
-        days += d2 + 1
-    return days
-
-days = count_days(m1, d1, m2, d2)
-weekday_index = (start_weekday + days) % 7
-weekday = weekdays[weekday_index]
-print(weekday)
+print(weekdays[diff % 7])
