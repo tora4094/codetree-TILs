320f9a63dcdd173beaf7b2b76dc147c54b488c6e
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-07 21:56:08 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240507/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240507/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -7,8 +7,8 @@ answer = [
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
-def in_range(x,y):
-    return 0 <= x < n and 0 <= y < n
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
 x, y = 0, 0
 dir_num = 0
