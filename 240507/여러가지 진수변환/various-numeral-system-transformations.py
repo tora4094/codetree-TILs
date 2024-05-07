7b0414c353a3c03ac16955ca903a7b25e6a105c7
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-07 14:23:29 +0900

    Title: 여러가지 진수변환, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/여러가지 진수변환/various-numeral-system-transformations.py
+++ 240507/여러가지 진수변환/various-numeral-system-transformations.py
@@ -1,11 +1,25 @@
-N,B = tuple(map(int,input().split()))
+N, B = tuple(map(int,input().split()))
+digits = []
 
-def convert(N, B):
-    result = ''
-    while N > 0:
-        remain = N % B
-        result = str(remain) + result
-        N //= B
-    return result
+while True:
+    if N < B:
+        digits.append(N)
+        break
+    digits.append(N % B)
+    N //= B
 
-print(convert(N,B))
+for digit in digits[::-1]:
+    print(digit, end="")
+
+
+# N,B = tuple(map(int,input().split()))
+
+# def convert(N, B):
+#     result = ''
+#     while N > 0:
+#         remain = N % B
+#         result = str(remain) + result
+#         N //= B
+#     return result
+
+# print(convert(N,B))
