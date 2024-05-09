23f0bbfb57681485b20a173d10aaec9e642213e1
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-09 18:52:39 +0900

    Title: 격자 위의 편안한 상태, Time: 303ms, Memory: 26MB, Status: Passed - CodeTree

--- 240509/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240509/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,28 +1,24 @@
-N, M = tuple(map(int,input().split()))
+N, M = map(int, input().split())
 
-arr = [
-    [0] * (N + 1)
-    for _ in range(N+1)
-]
+arr = [[0] * (N + 1) for _ in range(N + 1)]
 
-dxs, dys = [0,1,0,-1],[1,0,-1,0]
+dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
 
-def in_range(x,y):
+def in_range(x, y):
     return 1 <= x <= N and 1 <= y <= N
 
-def adjacent_cnt(x,y):
-    cnt = 0    
-    for dx, dy in zip(dxs,dys):
-        nx, ny = x + dx, y + dy        
-        if in_range(nx,ny) and arr[nx][ny] == 1:
+def adjacent_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr[nx][ny] == 1:
             cnt += 1
-    return cnt 
+    return cnt
 
 for _ in range(M):
-    r, c = tuple(map(int,input().split()))    
-    if in_range(r,c):
-        arr[r][c] = 1    
-        if adjacent_cnt(r,c) >= 3:
-            print(1)
-        else:
-            print(0)
+    r, c = map(int, input().split())
+    arr[r][c] = 1
+    if adjacent_cnt(r, c) == 3:
+        print(1)
+    else:
+        print(0)
