14f574aec0e34529ca8e0050b5dd3e2aeeffda9d
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-09 15:57:18 +0900

    Title: 격자 위의 편안한 상태, Time: 203ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240509/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240509/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -11,18 +11,18 @@ def in_range(x,y):
     return 1 <= x <= N and 1 <= y <= N
 
 def adjacent_cnt(x,y):
-    cnt = 0
-    result = 0
+    cnt = 0    
     for dx, dy in zip(dxs,dys):
         nx, ny = x + dx, y + dy        
         if in_range(nx,ny) and arr[nx][ny] == 1:
             cnt += 1
-    if cnt >= 3:
-        result = 1
-    return result
+    return cnt 
 
 for _ in range(M):
     r, c = tuple(map(int,input().split()))    
     if in_range(r,c):
         arr[r][c] = 1    
-        print(adjacent_cnt(r,c))
+        if adjacent_cnt(r,c) >= 3:
+            print(1)
+        else:
+            print(0)
