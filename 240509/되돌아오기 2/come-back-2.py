2ac4cc6923dc3dd73d9263adc2fa6522663bc67d
tora4094 <161473029+tora4094@users.noreply.github.com>
2024-05-09 15:01:15 +0900

    Title: 되돌아오기 2, Time: 138ms, Memory: 73MB, Status: Passed - CodeTree

--- 240509/되돌아오기 2/come-back-2.py
+++ 240509/되돌아오기 2/come-back-2.py
@@ -8,16 +8,15 @@ result = -1
 
 dir_num = 3 
 
-for d in A:
-    if cnt != 0 and x == 0 and y == 0:
-        result = cnt
-        break
+for d in A:    
     if d == 'F':
-        x, y = x + dxs[dir_num], y + dys[dir_num]        
+        x, y = x + dxs[dir_num], y + dys[dir_num]                
     elif d == 'L':
-        dir_num = (dir_num - 1) % 4
+        dir_num = (dir_num - 1 + 4) % 4
     elif d == 'R':
         dir_num = (dir_num + 1) % 4
     cnt += 1
-
+    if x == 0 and y == 0:
+        result = cnt        
+        break
 print(result)
